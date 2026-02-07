9ddad6f095ccc984ec845cca3497a886ad1bad57
Manoj <manojvrgowda2004@gmail.com>
2026-02-07 21:17:00 +0530

    intro to js

---
+++ script.js
@@ -0,0 +1,33 @@
+let name = "John";
+let age = 21;
+console.log("MY age is",age);
+console.log("MY name is ",name);
+
+let a=100;
+let b=200;
+// let sum = a+b;
+// console.log("The sum of a and b is ",sum);
+// let sub = a-b;
+// console.log("The sub of a and b is ",sub);
+// let mul = a*b;
+// console.log("The mul of a and b is ",mul);
+// let div = a/b;
+// console.log("The div of a and b is ",div);
+console.log("The sum of a and b is ",a+b);
+console.log("The sub of a and b is ",a-b);
+console.log("The mul of a and b is ",a*b);
+console.log("The div of a and b is ",a/b);
+
+
+let isElibleForVote = true;
+console.log("Is John elible for vote?",isElibleForVote);
+
+
+let title = "JavaScript";
+javaScript = "intro to JavaScript";
+console.log(title);
+console.log(javaScript);
+
+// const values cant be changed
+const office = 'Manoj Office';
+console.log(office);

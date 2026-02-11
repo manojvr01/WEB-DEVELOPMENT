329dc0186be2570d6394baa65fbc0343e731750b
Manoj <manojvrgowda2004@gmail.com>
2026-02-11 12:19:29 +0530

    operators in js

---
+++ operator.js
@@ -0,0 +1,65 @@
+//assignment operator
+const a=9099;
+const b=9099;
+//arithmetic operators
+console.log(a+b); 
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+//increment and decrement operators
+let c=10;   //const variables cannot be reassigned, so we use let for c to allow incrementing and decrementing
+c++;   
+console.log(c);
+c--;
+console.log(c);
+//pre-increment and post-increment
+let d=5;
+console.log(d);
+console.log(++d);
+console.log(d);
+console.log(d++);
+console.log(d);
+//pre-decrement and post-decrement
+let e=5;
+console.log(e);
+console.log(--e);
+console.log(e);
+console.log(e--);
+console.log(e);
+//exponentiation operator
+let f=2;
+let g=3;
+console.log(f**g); //2 raised to the power of 3
+//shorrt hand assignment operators
+let h=10;
+h+=5;
+console.log(h);
+h-=3;
+console.log(h); 
+h*=2;
+console.log(h);
+h/=4;   
+console.log(h);
+h%=3;
+console.log(h); 
+h**=2;
+console.log(h);
+//comparison operators
+let i=10;
+let j=20;
+console.log(i===j);
+console.log(i==j);
+console.log(i!=j);
+console.log(i>j);
+console.log(i<j);
+console.log(i>=j);
+console.log(i<=j);
+//logical operators
+let k=true;
+let l=false;
+console.log(k && l);
+console.log(k || l);
+console.log(!k);
+console.log(!l);
+

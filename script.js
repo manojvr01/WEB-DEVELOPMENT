f2618fdba91e75c8d03789c965c947682b79d70c
Manoj <manojvrgowda2004@gmail.com>
2026-02-11 10:32:14 +0530

    Data types in Js

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-let name = "John";
-let age = 21;
-console.log("MY age is",age);
-console.log("MY name is ",name);
+let a=999;
+// a="manoj";
+a=true;
+let d=99;
+console.log("d is ",typeof(d));
+let c=null;
+console.log("c is ",typeof(c));
+ console.log("a is ",a );
+ console.log("type of a is ",typeof(a));
 
-let a=100;
-let b=200;
-// let sum = a+b;
-// console.log("The sum of a and b is ",sum);
-// let sub = a-b;
-// console.log("The sub of a and b is ",sub);
-// let mul = a*b;
-// console.log("The mul of a and b is ",mul);
-// let div = a/b;
-// console.log("The div of a and b is ",div);
-console.log("The sum of a and b is ",a+b);
-console.log("The sub of a and b is ",a-b);
-console.log("The mul of a and b is ",a*b);
-console.log("The div of a and b is ",a/b);
+ let v1=15;
+ console.log(v1 +23); 
+
+ let v2="15";
+console.log(v2 +23);  // here 23 is converted to string and concatenated with "15" and result is "1523"
+
+//let result=null;
+let z=1234;
+//result=String(z);
+z=String(z);
+console.log("z is ",z);
+console.log("type of z is ",typeof(z));
 
 
-let isElibleForVote = true;
-console.log("Is John elible for vote?",isElibleForVote);
+//let  m="1234";
+let m="afgbh";  //naN represents not a number
+m=Number(m);
+console.log("m is ",m);
+console.log("type of m is ",typeof(m));
 
-
-let title = "JavaScript";
-javaScript = "intro to JavaScript";
-console.log(title);
-console.log(javaScript);
-
-// const values cant be changed
-const office = 'Manoj Office';
-console.log(office);
+//let p=1234;
+let p=0;   //only 0 is converted to false and all other numbers are converted to true
+p=Boolean(p);
+console.log("p is ",p);
+console.log("type of p is ",typeof(p));

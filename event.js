a6ed8ab8d28933cef3e730ed8a1d14c62a5801f4
Manoj <manojvrgowda2004@gmail.com>
2026-02-26 12:24:08 +0530

    evens in js

---
+++ event.js
@@ -0,0 +1,34 @@
+//event
+
+
+
+//event handlers
+//(onclick, onmouseover, onmouseout, onkeydown, onkeyup, onkeypress, onload, onunload, onfocus, onblur, onchange, onsubmit, onreset,onkeydown, onkeypress, onkeyup, onmousedown, onmouseup, onmousemove, onmouseover, onmouseout, onwheel,onscroll, onresize, onerror, oncontextmenu, onselect, oninput, ondrag, ondrop, ondragstart, ondragend, ondragenter, ondragleave, ondragover,oncopy, oncut, onpaste, onabort, oncanplay, oncanplaythrough, ondurationchange, onemptied, onended, onerror, onloadeddata, onloadedmetadata, onpause, onplay, onplaying, onprogress, onratechange, onseeked, onseeking, onstalled, onsuspend, ontimeupdate, onvolumechange, onwaiting)
+
+
+
+//onclick event is triggered when the user clicks on an element. It can be used to perform an action when a button is clicked, a link is clicked, or any other element is clicked.
+// function onclickfunc(){
+//     console.log("onclick event is triggered");
+// }
+
+//onmouseover event is triggered when the user moves the mouse pointer over an element. It can be used to change the appearance of an element when the mouse is over it, or to display additional information.
+
+// const keypressEvent= () =>{
+//   console.log("keypress event is triggered");  
+// }
+
+
+//event listerns
+
+  // const box1=document.getElementById('box-1');
+  // box1.addEventListener('click',()=>{
+//   console.log("click has beeen triggerd")
+// })  //everything is in js no html input required
+
+function firstfunc(){
+  console.log("func");
+}
+function secondfunc(){
+  console.log("func2");
+}
